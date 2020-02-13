ba929da1234d531458fe10785847925bc4d3180a
Ian Roberts <irob466@gmail.com>
2020-02-12 20:26:09 -0600

    this program does a thing.

---
+++ README.md
@@ -0,0 +1,44 @@
+# w2d f4r
+
+For when your quest for a11y has a few c7g n8s that you need to d4e.
+
+## How Do I Do That?
+
+```bash
+yarn install # or npm i
+node wordSearch {your numeronym}
+```
+
+## What Does That Return?
+
+It returns an array of all the words that could possibly fit your numeronym. For example:
+
+```bash
+node wordSearch w8t
+```
+
+returns:
+
+```javascript
+[
+  "wainwright",
+  "wanderlust",
+  "watercraft",
+  "waterfront",
+  "watersport",
+  "waterspout",
+  "watertight",
+  "watervliet",
+  "whereabout",
+  "whirlabout",
+  "whiteprint",
+  "wildebeest",
+  "winningest",
+  "witchcraft",
+  "wonderment",
+  "woollybutt",
+  "woonsocket",
+  "workbasket",
+  "wykehamist"
+];
+```
---
+++ wordSearch.js
@@ -0,0 +1,35 @@
+const axios = require("axios");
+const [, , input] = process.argv;
+
+const API_ENDPOINT = "https://api-portal.dictionary.com/dcom/list/";
+
+const findWords = async numer => {
+  if (!/\w\d+\w/i.test(numer)) {
+    console.error(
+      "That ain't right. Your numeronym has to be in the form of {letter} {number} {letter}"
+    );
+    return null;
+  }
+  const offset = 0;
+  const limit = 10000;
+  const letter = numer[0];
+  const lastLetter = numer.slice(-1);
+  const length = numer.slice(1, -1);
+  const re = new RegExp(`^${letter}\\w{${length}}${lastLetter}$`, "i");
+
+  try {
+    const {
+      data: { data }
+    } = await axios.get(
+      `${API_ENDPOINT}${letter}?offset=${offset}&limit=${limit}`
+    );
+    const res = data
+      .filter(({ displayForm }) => re.test(displayForm))
+      .map(({ displayForm }) => displayForm);
+    console.log(res);
+  } catch (err) {
+    throw err;
+  }
+};
+
+findWords(input);
